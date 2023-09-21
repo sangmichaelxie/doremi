2e52ce36f0de10595e28662fa7417e7e5bfff3fa
Michael Xie <xie@cs.stanford.edu>
2023-09-20 21:43:12 -0700

    use flash 2.0.0

--- scripts/setup_flash.sh
+++ scripts/setup_flash.sh
@@ -5,8 +5,7 @@ source constants.sh
 pip install -e .
 pip uninstall -y ninja && pip install ninja
 git clone git@github.com:/HazyResearch/flash-attention
-cd ${DOREMI_DIR}/flash-attention && git checkout v2.0.4
-cd ${DOREMI_DIR}/flash-attention && rm flash_attn/models/falcon.py
+cd ${DOREMI_DIR}/flash-attention && git checkout v2.0.0
 cd ${DOREMI_DIR}/flash-attention && python setup.py install
 cd ${DOREMI_DIR}/flash-attention/csrc/fused_dense_lib && pip install .
 cd ${DOREMI_DIR}/flash-attention/csrc/xentropy && pip install .
